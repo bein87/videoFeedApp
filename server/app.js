919d8477f7daddb11b34213f3555858f8f36f1e1
tzach <tzach@jumbomail.me>
2020-08-16 13:04:53 +0300

    minor ui changes

--- server/app.js
+++ server/app.js
@@ -2,20 +2,14 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cors = require("cors");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 // app setup
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 //enabling cors
 app.use(cors());
